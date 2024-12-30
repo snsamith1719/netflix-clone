d33649b7d0e568f0748926e7ce5d696d6b6151a1
snsamith1719 <samithrakshan@gmail.com>
2024-12-30 21:11:26 +0530

    Fetching Movies from API, by trending, trailers,details,similar,by category

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -7,6 +7,7 @@ const ENV_VARS = {
   PORT: process.env.PORT || 3000,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
+  TMDB_API_KEY: process.env.TMDB_API_KEY,
 };
 
 export default ENV_VARS;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // const express = require('express'); // CommonJS
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import movieRoutes from "./routes/movieRoutes.js";
 import ENV_VARS from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
@@ -11,8 +12,7 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
-
-console.log("Mongo URI: ", ENV_VARS.MONGO_URI);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
