40b9e39ae8cb90aa2b84e8e6461282fa3b9d1cee
snsamith1719 <samithrakshan@gmail.com>
2024-12-30 23:41:23 +0530

    Fetch TV shows via API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/authRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
 import ENV_VARS from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import tvRoutes from "./routes/tvRoutes.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
